07492b978e76d2cefbc23cc28641ff8d784938a8
Luciano <lucianooviedo4@gmail.com>
2025-12-10 07:13:58 -0300

    configuración deploy en render

--- models/config.js
+++ models/config.js
@@ -14,7 +14,9 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
+    host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
     logging: false,
   }
 );
